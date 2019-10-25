813e8ccc00dc31bd74151005901375b1eac94ea0
mgandhe <mgandhe@RENTLAPT0099>
2019-10-25 13:30:03 +0530

    Developed Hotels API

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,18 @@ namespace ProjectAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                    });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -39,8 +51,8 @@ namespace ProjectAPI
             {
                 app.UseHsts();
             }
-
-             app.UseMvc();
+            app.UseCors("AllowAll");
+            app.UseMvc();
         }
     }
 }
